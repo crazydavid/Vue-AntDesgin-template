41bd6d94d7db5a098a66df320e3a33de99ab4b3b
chensiqiao1993 <996993166@qq.com>
2020-12-10 23:33:09 +0800

    登录页提交

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -8,52 +8,19 @@
       :rules="state.rules"
       @submit="handleSubmit"
     >
-      <ant-tabs
-        :active-key="customActiveKey"
-        :tab-bar-style="{ textAlign: 'center', borderBottom: 'unset' }"
-        @change="handleTabClick"
-      >
-        <ant-tab-pane key="tab1" tab="账号密码登录">
-          <ant-form-item v-bind="validateInfos.username">
-            <ant-input v-model:value="userForm.username" size="large" type="text" placeholder="账户: admin">
-              <template #prefix><user-outlined :style="{ color: 'rgba(0,0,0,.25)' }" type="user" /></template>
-            </ant-input>
-          </ant-form-item>
-          <ant-form-item v-bind="validateInfos.password">
-            <ant-input-password v-model:value="userForm.password" size="large" placeholder="密码: admin or ant.design">
-              <template #prefix><LockOutlined :style="{ color: 'rgba(0,0,0,.25)' }" type="lock" /></template>
-            </ant-input-password>
-          </ant-form-item>
-        </ant-tab-pane>
-        <ant-tab-pane key="tab2" tab="手机号登录">
-          <ant-form-item v-bind="validateInfos.mobile">
-            <ant-input :value="userForm.mobile" size="large" type="text" placeholder="手机号">
-              <template #prefix><MobileOutlined :style="{ color: 'rgba(0,0,0,.25)' }" type="mobile" /></template>
-            </ant-input>
-          </ant-form-item>
-          <ant-row :gutter="16">
-            <ant-col class="gutter-row" :span="16">
-              <ant-form-item v-bind="validateInfos.captcha">
-                <ant-input :value="userForm.captcha" size="large" type="text" placeholder="验证码">
-                  <template #prefix><MailOutlined :style="{ color: 'rgba(0,0,0,.25)' }" type="captcha" /></template>
-                </ant-input>
-              </ant-form-item>
-            </ant-col>
-            <ant-col class="gutter-row" :span="8">
-              <ant-button
-                class="getCaptcha"
-                tabindex="-1"
-                :disabled="state.formState.smsSendBtn"
-                @click.stop.prevent="getCaptcha"
-                v-text="!state.formState.smsSendBtn && '获取验证码' || (state.formState.time+' s')"
-              />
-            </ant-col>
-          </ant-row>
-        </ant-tab-pane>
-      </ant-tabs>
+      <ant-form-item v-bind="validateInfos.username">
+        <ant-input v-model:value="userForm.username" size="large" type="text" placeholder="账户: admin">
+          <template #prefix><user-outlined :style="{ color: 'rgba(0,0,0,.25)' }" type="user" /></template>
+        </ant-input>
+      </ant-form-item>
+      <ant-form-item v-bind="validateInfos.password">
+        <ant-input-password v-model:value="userForm.password" size="large" placeholder="密码: admin or ant.design">
+          <template #prefix><LockOutlined :style="{ color: 'rgba(0,0,0,.25)' }" type="lock" /></template>
+        </ant-input-password>
+      </ant-form-item>
 
       <ant-form-item>
-        <ant-checkbox :checked="state.rememberMe">自动登录</ant-checkbox>
+        <ant-checkbox v-model:checked="state.rememberMe">自动登录</ant-checkbox>
         <router-link
           :to="{ name: 'login' }"
           class="forge-password"
@@ -80,15 +47,13 @@
 </template>
 <script lang="ts">
 import { defineComponent, reactive, toRaw, ref } from 'vue'
-import { Form, Tabs, Input, Row, Col, Button, Checkbox } from 'ant-design-vue'
+import { Form, Input, Button, Checkbox } from 'ant-design-vue'
 import { useForm } from '@ant-design-vue/use'
-import { UserOutlined, LockOutlined, MobileOutlined, MailOutlined } from '@ant-design/icons-vue'
+import { UserOutlined, LockOutlined, MobileOutlined } from '@ant-design/icons-vue'
 
 interface userLoginInfo {
     username?:string | number,
     password?:string | number,
-    mobile?:number,
-    captcha?:number
 }
 
 export default defineComponent({
@@ -96,16 +61,11 @@ export default defineComponent({
     UserOutlined,
     LockOutlined,
     MobileOutlined,
-    MailOutlined,
     'ant-form': Form,
     'ant-form-item': Form.Item,
-    'ant-tabs': Tabs,
-    'ant-tab-pane': Tabs.TabPane,
     'ant-input': Input,
     'ant-input-password': Input.Password,
     'ant-button': Button,
-    'ant-row': Row,
-    'ant-col': Col,
     'ant-checkbox': Checkbox
   },
   setup() {
@@ -114,19 +74,13 @@ export default defineComponent({
        */
     const userForm = reactive<userLoginInfo>({
       username: '',
-      password: '',
-      mobile: undefined,
-      captcha: undefined
+      password: ''
     })
     const customActiveKey = ref('tab1')
-    const formRules = reactive(
-      customActiveKey.value === 'tab1' ? {
-        username: [{ required: true, message: '请输入账号', trigger: 'change' }],
-        password: [{ required: true, message: '请输入密码', trigger: 'change' }]
-      } : {
-        mobile: [{ required: true, pattern: /^1[34578]\d{9}$/, message: '请输入正确的手机号', trigger: 'change' }],
-        captcha: [{ required: true, message: '请输入验证码', trigger: 'change' }]
-      })
+    const formRules = reactive({
+      username: [{ required: true, message: '请输入账号', trigger: 'change' }],
+      password: [{ required: true, message: '请输入密码', trigger: 'change' }]
+    })
     /**
      * 其他信息
      */
@@ -144,12 +98,15 @@ export default defineComponent({
     /**
      * tab切换
      */
-    const handleTabClick = (key:string) => { customActiveKey.value = key }
+    const { resetFields, validate, validateInfos } = useForm(userForm, formRules)
+    const handleTabClick = (key:string) => {
+      customActiveKey.value = key
+      resetFields()
+    }
     const getCaptcha = () => {}
     /**
      * 提交表单
      */
-    const { resetFields, validate, validateInfos } = useForm(userForm, formRules)
     const handleSubmit = (e:any) => {
       e.preventDefault()
       validate().then(() => {
